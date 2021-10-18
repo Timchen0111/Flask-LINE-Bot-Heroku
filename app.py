3a5785e97553b96fa80407c876e10081fa4f6d64
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2021-10-18 15:59:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,5 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
+    reply = TextSendMessage(text=f"{get_message}")+"喵"
     line_bot_api.reply_message(event.reply_token, reply)
-    
-    if get_message = "壞貓貓":
-        line_bot_api.reply_message(event.reply_token,"你才壞貓貓")
