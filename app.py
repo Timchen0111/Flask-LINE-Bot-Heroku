ff6e18db2c3ecce1498933833c386d47455f50e1
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2021-10-23 15:12:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def updateMember(id,j):
     cursor.execute('SELECT "lineID" FROM member WHERE "lineID" = \'%s\''%str(id))
     query = cursor.fetchall()
     if query != []:
-        print("使用者已登入")
+        #print("使用者已登入")
         pass
     else:
         cursor.execute('SELECT MAX("memberNumber") FROM member')
@@ -65,7 +65,7 @@ def updateMember(id,j):
         if maxnum == None:
             maxnum = 0
         memberNum = maxnum+1 
-        print(type(j))
+        #print(type(j))
         name = j["memberName"]
         phone = j["phone"]   
         cursor.execute("INSERT INTO member VALUES(%s,%s,%s,%s);",(memberNum,name,phone,id))
@@ -75,16 +75,16 @@ def updateMember(id,j):
 def updateProduct(id,j):
     cursor.execute("SELECT \"memberNumber\" From member WHERE \"lineID\" = '%s'"%str(id))
     query = cursor.fetchall()
-    print(query)
+    #print(query)
     memberNum = query[0][0]
     cursor.execute('SELECT MAX("productNumber") FROM product')
     query = cursor.fetchall()
     maxnum = query[0][0]
-    print(maxnum)
+    #print(maxnum)
     if maxnum == None:
         maxnum = 0
     proNum = maxnum+1
-    print(proNum)
+    #print(proNum)
     cursor.execute('INSERT INTO product("productNumber","memberNumber","productName","productDescription","productPicturelink","productPrice","productQuantity","deliveryPlace") VALUES(%s,%s,%s,%s,%s,%s,%s,%s);',(proNum,memberNum,j["name"],j["description"],j["link"],j["price"],j["quantity"],j["place"]))
     conn.commit()
 
