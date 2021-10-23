1324530937d222c81c061908d4e01c64bcce3e4a
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2021-10-23 14:01:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ import psycopg2
 from flask import Flask, abort, request
 
 #連接資料庫
-DATABASE_URL = os.environ['postgres://xseaswlvhvhgnm:a6383e19f7ab5a17b0b89671e2d8c363ce18a229550faaac57d61058e8269929@ec2-34-233-64-238.compute-1.amazonaws.com:5432/de3mlq5i95dhst']
+DATABASE_URL = 'postgres://xseaswlvhvhgnm:a6383e19f7ab5a17b0b89671e2d8c363ce18a229550faaac57d61058e8269929@ec2-34-233-64-238.compute-1.amazonaws.com:5432/de3mlq5i95dhst'
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = conn.cursor()
-
 #連linebot
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
@@ -37,87 +36,16 @@ def callback():
 
         return "OK"
 
-#把使用者上架傳送資料製造成Dictionary
-def updateDictionary(text):
-    lst = text.split("\n")
-    myDict = {}
-    myDict["memberName"] = lst[0]
-    myDict["phone"] = lst[1]
-    myDict["name"] = lst[3] 
-    myDict["description"] = lst[4]
-    myDict["link"] = lst[5]
-    myDict["price"] = int(lst[6])
-    myDict["quantity"] = int(lst[7])
-    myDict["place"] = lst[8]
-    return myDict
 
-#使用者登入函式
-def updateMember(id,j):
-    cursor.execute('SELECT "lineID" FROM member WHERE "lineID" = \'%s\''%str(id))
-    query = cursor.fetchall()
-    if query != []:
-        print("使用者已登入")
-        pass
-    else:
-        cursor.execute('SELECT MAX("memberNumber") FROM member')
-        query = cursor.fetchall()
-        maxnum = query[0][0]
-        if maxnum == None:
-            maxnum = 0
-        memberNum = maxnum+1 
-        print(type(j))
-        name = j["memberName"]
-        phone = j["phone"]   
-        cursor.execute("INSERT INTO member VALUES(%s,%s,%s,%s);",(memberNum,name,phone,id))
-        conn.commit()
-
-#上架函式
-def updateProduct(id,j):
-    cursor.execute("SELECT \"memberNumber\" From member WHERE \"lineID\" = '%s'"%str(id))
-    query = cursor.fetchall()
-    print(query)
-    memberNum = query[0][0]
-    cursor.execute('SELECT MAX("productNumber") FROM product')
-    query = cursor.fetchall()
-    maxnum = query[0][0]
-    print(maxnum)
-    if maxnum == None:
-        maxnum = 0
-    proNum = maxnum+1
-    print(proNum)
-    cursor.execute('INSERT INTO product("productNumber","memberNumber","productName","productDescription","productPicturelink","productPrice","productQuantity","deliveryPlace") VALUES(%s,%s,%s,%s,%s,%s,%s,%s);',(proNum,memberNum,j["name"],j["description"],j["link"],j["price"],j["quantity"],j["place"]))
-    conn.commit()
-
-
-#處理訊息
-def text_reply(content, event):
-    reply = TextSendMessage(text=content)
-    line_bot_api.reply_message(event.reply_token, reply)
 
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    id = event.source.user_id  # 獲取使用者ID
-    #print(id)
-    get_message = event.message.text.rstrip().strip()
-    if get_message == "下單":
-        errortext = "這邊還沒開發好qq"
-        text_reply(errortext,event)
-    elif get_message[:2] == "上架":
-        d = updateDictionary(get_message)
-        #print(type(d))
-        updateMember(id,d)
-        updateProduct(id,d)
-        finish = "上架完成！"
-        text_reply(finish,event)        
-    else:
-        confuse = "我聽不懂你在說什麼"
-        text_reply(confuse,event)
+    confuse = "我就爛"
+    text_reply(confuse,event)
 
-conn.commit()
-cursor.close()
-conn.close()
+
 
         
         
