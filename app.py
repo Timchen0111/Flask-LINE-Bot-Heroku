60f4a87696bf8eca19fd9ed6d6a0b8b961844ced
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2021-10-23 14:02:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 from datetime import datetime
-from typing import Text
-import psycopg2
+
 from flask import Flask, abort, request
 
-#連接資料庫
+import psycopg2
 DATABASE_URL = 'postgres://xseaswlvhvhgnm:a6383e19f7ab5a17b0b89671e2d8c363ce18a229550faaac57d61058e8269929@ec2-34-233-64-238.compute-1.amazonaws.com:5432/de3mlq5i95dhst'
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = conn.cursor()
-#連linebot
+
+
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -37,14 +37,13 @@ def callback():
         return "OK"
 
 
-
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    confuse = "我就爛"
-    text_reply(confuse,event)
+    get_message = event.message.text
 
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
 
 
         
